cd9835dc0c0b8a25f5b4df1a1be13d10332bfb34
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-06 18:05:08 +0000

    Deletion of loop and variables, print with char precision

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split()
-display = [words[i] for i in [5, 6, 12, 0]]
-print(' '.join(display))
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
