124b46c2a25071d268867aecfef414fbd71058a7
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-06 18:57:48 +0000

    Replace last elif by else

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,12 +2,12 @@
 
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
+
 if number > 0:
     print(f"{number} is positive")
 
 elif number == 0:
     print(f"{number} is zero")
 
-elif number < 0:
+else:
     print(f"{number} is negative")
