8f5554dd2537d4116dc05243e6cdfaed4bb31d5b
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-07 10:40:36 +0000

    Deletion of comment

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,2 @@
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-
-"""Test de la fonction
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('z'))  # True
-print(islower('Z'))  # False
-print(islower('5'))  # False"""
