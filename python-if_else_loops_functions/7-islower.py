ebacc3b83480b8db49f1b1313778126c32a666c7
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-07 10:37:18 +0000

    Function checks for lowercase character

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+def islower(c):
+    return ord('a') <= ord(c) <= ord('z')
+
+"""Test de la fonction
+print(islower('a'))  # True
+print(islower('A'))  # False
+print(islower('z'))  # True
+print(islower('Z'))  # False
+print(islower('5'))  # False"""
