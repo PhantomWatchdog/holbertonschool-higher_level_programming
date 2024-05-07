1938bc4146d9760103c4fbcbf3740defdf577116
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-07 14:37:45 +0000

    Use of string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) < 123:
-            print("{}".format(chr(ord(c) - 32)), end='')
-
+            c = chr(ord(c) - 32)
+            print("{}".format(c), end='')
         else:
             print("{}".format(c), end='')
     print("")
