bbdf91c24ce8400179860c727a7e556437223ead
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-13 11:18:17 +0000

    Fix last space display

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=' ')
-        print()
+        #for num in row:
+        #print("{:d}".format(num), end=' ') #Display a space at end
+        print(" ".join(["{:d}".format(num) for num in row]))
