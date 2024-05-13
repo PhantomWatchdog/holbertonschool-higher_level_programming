c4addc2db55eb5e986be79944dfec19c7c8f45b7
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-13 08:14:00 +0000

    Remove variable declaration

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    listing = my_list
-    for item in listing:
+    for item in my_list:
         if idx < 0:
             return None
-        elif idx > len(listing):
+        elif idx > len(my_list):
             return None
         else:
             return idx + 1
