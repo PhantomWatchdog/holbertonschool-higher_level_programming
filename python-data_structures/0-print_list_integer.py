c1238c915b0bc5b2be849c2e4c5573f9f68a73f3
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-13 08:02:44 +0000

    Remove variable declaration

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    listing = my_list
-    for item in listing:
+    for item in my_list:
         print("{}".format(item))
---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    listing = my_list
+    for item in listing:
+        if idx < 0:
+            return None
+        elif idx > len(listing):
+            return None
+
+        print("{}".format(listing))
