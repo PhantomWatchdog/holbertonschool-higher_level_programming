293a6b7a5b467c8561dea1e7c37ecb474b95e91e
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-14 10:08:41 +0000

    Modification of sort attribut

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     '''for key, value in sorted(a_dictionary.items()):
-        print(key, ':', value)
+            print(key, ':', value)
     '''
-    sorted_keys = sorted(a_dictionary.keys())
+    sorted_keys = sorted(a_dictionary)
 
     for key in sorted_keys:
         print(key, ':', a_dictionary[key])
