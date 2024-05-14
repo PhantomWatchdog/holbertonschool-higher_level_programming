61141457dbb64d3788f6095f9b825f6b34a474ed
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-14 12:11:48 +0000

    Return a new dictionary with all values multiplied by 2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    '''new_list = {}
+    for key, value in a_dictionary.items():
+        new_list[key] = value * 2
+    return new_list
+    '''
+
+    return {key: value * 2 for key, value in a_dictionary.items()}
