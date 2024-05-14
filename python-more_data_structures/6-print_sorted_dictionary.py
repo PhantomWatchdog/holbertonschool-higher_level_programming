ddba767e9483d7350bf5f4b4983b0b2139b12094
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-14 10:34:07 +0000

    Testing different code format

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    '''for key, value in sorted(a_dictionary.items()):
-            print(key, ':', value)
+    '''for key in sorted(a_dictionary.keys()):
+        value = a_dictionary[key]
+        print("{}: {}".format(key, value))
     '''
-    sorted_keys = sorted(a_dictionary)
-
-    for key in sorted_keys:
-        print(key, ':', a_dictionary[key])
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
