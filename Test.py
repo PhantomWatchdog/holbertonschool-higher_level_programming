953af133bfe968392a3fde121325727e3489ea2b
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-15 21:05:36 +0200

    Test of debugging tools with breakpoints and log messages

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+print("Let's Computes!")
+"""_print function_: This function prints the string "Hello World!" to the console.
+"""
+
+def addition(a,b):
+    return a+b
+
+print(addition(2,3))
