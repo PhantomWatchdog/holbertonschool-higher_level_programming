3987423c92291010590f8bf86d32e71bc418d7a6
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-17 14:21:32 +0200

    Fix pycodestyle issues

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-  """
-  Safely divides two numbers and prints the result.
+    """
+    Safely divides two numbers and prints the result.
 
-  Args:
-    a (float): The numerator.
-    b (float): The denominator.
+    Args:
+        a (float): The numerator.
+        b (float): The denominator.
 
-  Returns:
-    float: The result of the division, or None if the denominator is zero.
-  """
-  try:
-    result = a / b
-  except ZeroDivisionError:
-    result = None
-  finally:
-    print("Inside result: {}".format(result))
-  return result
-  
+    Returns:
+        float: The result of the division, or None if the denominator is zero.
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
