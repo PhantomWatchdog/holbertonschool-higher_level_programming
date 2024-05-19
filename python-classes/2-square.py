eb9ecaaa92c3be20a51aedc11e1909cc00b4786a
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 14:42:43 +0200

    Fix pycodestyle issues

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -29,8 +29,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
         """Returns the current square area."""
         return (self.__size) ** 2
-    
