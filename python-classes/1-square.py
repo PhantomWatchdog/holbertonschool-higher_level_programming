fef938ea0c2bcca4ed322f17b88416e47f036d5a
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 14:01:51 +0200

    Create init method

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a module for a class Square.
+"""
+
+
+class Square:
+    """
+    This is a class representing a square.
+    """
+    def __init__(self, size):
+        self.__size = size
