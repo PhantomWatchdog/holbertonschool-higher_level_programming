5f305991fcd8e4872cbf032b6f7960c6b5ea247a
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 18:02:41 +0200

    Change position area

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -24,21 +24,8 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
-
-    def area(self):
-        """
-        Returns the current square area.
-
-        Returns:
-            int: The area of the square.
-        """
-        return (self.__size) ** 2
 
     @property
     def size(self):
@@ -90,6 +77,15 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    def area(self):
+        """
+        Returns the current square area.
+
+        Returns:
+            int: The area of the square.
+        """
+        return (self.__size) ** 2
 
     def my_print(self):
         """
