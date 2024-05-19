b9a6deeb1fb6668735a67e39f1be9930dcc4bb69
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 17:52:36 +0200

    Test n°11

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -83,28 +83,25 @@ class Square:
         Raises:
             TypeError: If value is not a tuple of 2 positive integers.
         """
-        if not isinstance(value, tuple):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
+        elif not all(isinstance(val, int) and val >= 0 for val in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def my_print(self):
         """
         Prints in stdout the square with the character #.
         """
-        if self.__size == 0:
-            print("")
-        else:
-            for i in range(0, self.__position[1]):
-                print("")
-            for i in range(0, self.__size):
-                for j in range(0, self.__position[0]):
+        if self.__size > 0:
+            for x in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for y in range(self.__position[0]):
                     print(" ", end="")
-                for k in range(0, self.__size):
-                    print("#", end="")
-                print("")
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
+        else:
+            print()
