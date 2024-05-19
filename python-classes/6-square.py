14f8d0cb8553cc9c4cf420e15c4e9963c13a0601
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 17:06:35 +0200

    Test n°4

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -94,7 +94,7 @@ class Square:
         Prints in stdout the square with the character #.
         """
         if self.__size == 0:
-            print()
+            print("")
         else:
             for _ in range(self.__position[1]):
                 print()
