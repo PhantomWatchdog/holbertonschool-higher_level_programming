a70dcb82d12a5cc48a852083e4c1b3f139d1f604
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 17:40:49 +0200

    Test n°11

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -98,9 +98,13 @@ class Square:
         Prints in stdout the square with the character #.
         """
         if self.__size == 0:
-            print()
+            print("")
         else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            for i in range(0, self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for j in range(0, self.__position[0]):
+                    print(" ", end="")
+                for k in range(0, self.__size):
+                    print("#", end="")
+                print("")
