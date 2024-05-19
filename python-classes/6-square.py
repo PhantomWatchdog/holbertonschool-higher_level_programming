3bb0be3f704077cfa67da49148540b79c64857ed
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 16:59:22 +0200

    Test n°3

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -23,11 +23,7 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """   
         self.__size = size
         self.__position = position
 
