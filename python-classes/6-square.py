24a40b839461144933f61a253783ffc371be5c91
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 16:39:22 +0200

    Replace spaces by underscores

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -101,5 +101,5 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print(" " * self.__position[0], end="")
+                print("_" * self.__position[0], end="")
                 print("#" * self.__size)
