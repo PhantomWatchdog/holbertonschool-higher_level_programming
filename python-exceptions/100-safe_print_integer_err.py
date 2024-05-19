1ef3bc5a51b2dc605eb02ef8303ce9da030d53da
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 20:11:27 +0200

    Add documentation and fix pycodestyle issues

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+    Prints an integer with "{:d}".format().
+"""
+
+
 import sys
 def safe_print_integer_err(value):
+    """
+    Safely prints an integer value.
+
+    Args:
+        value (int): The integer value to be printed.
+
+    Returns:
+        bool: True if the value was successfully printed, False otherwise.
+    """
     try:
         print("{:d}".format(value))
         return True
