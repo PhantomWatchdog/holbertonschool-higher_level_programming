a85de96ce71fe84053a735a8557b30bc17510d23
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 17:19:45 +0200

    Test n°8

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -96,8 +96,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        
-        for _ in range(self.__position[1]):
-            print("")
-        for _ in range(self.__size):
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
