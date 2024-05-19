9058dd5c3c3d89625f3a8ad990cf7daa4a402951
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-19 19:55:19 +0200

    Remove specifier format

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        print("{}".format(value))
         return True
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e))
