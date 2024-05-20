a781fc127cea22bbe4398428510cabe7c67667ff
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-20 13:10:05 +0200

    Reduce method docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,17 +9,6 @@
 def add_integer(a, b=98):
     """
     Add two integers.
-
-    Args:
-        a (int or float): The first integer.
-        b (int or float): The second integer. Defaults to 98.
-
-    Returns:
-        int: The sum of the two integers.
-
-    Raises:
-        TypeError: If either `a` or `b` is not an integer or float.
-
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
