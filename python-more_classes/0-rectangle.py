a9d44578e9d3a2fffd421d733cae98a6127270cc
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-21 13:38:53 +0200

    Add docstring

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+This is the "0-rectangle" module.
+"""
+
 
 class Rectangle:
     """
-    Rectangle class with no attributes
+    This is the Rectangle class.
+
+    It represents a rectangle with no attributes.
     """
     pass
