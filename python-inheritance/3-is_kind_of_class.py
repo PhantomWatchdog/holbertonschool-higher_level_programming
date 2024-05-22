144490311a6e38ccf7177394336aee76ed44ddd4
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-22 15:44:23 +0200

    Fix pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,8 @@ Module for is_kind_of_class method.
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if an object is an instance of a class or an instance of a class that inherited from the specified class.
+    Check if an object is an instance of a class or
+    an instance of a class that inherited from the specified class.
 
     Args:
         obj: The object to check.
