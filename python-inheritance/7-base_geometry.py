158dae4c5b5b04f573c2a1982de5b85355ab1a68
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-22 17:08:07 +0200

    Fix pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,7 +15,7 @@ class BaseGeometry:
         'area() is not implemented'.
         """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         Method that validates value.
