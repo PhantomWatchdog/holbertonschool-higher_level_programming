878780fee054eb459ba8080da282bde029c0e791
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-22 15:10:19 +0200

    Test ckecker

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -12,12 +12,6 @@ class MyList(list):
     A class that inherits from list.
     """
 
-    def __init__(self):
-        """
-        Initializes a MyList object.
-        """
-        pass
-
     def print_sorted(self):
         """
         Prints the list, but sorted.
