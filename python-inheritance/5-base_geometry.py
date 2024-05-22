b1ab545ddf42df539285ce50710dff958c4ea39f
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-22 15:51:47 +0200

    Add Task 5 and 6

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class with a method.
+    """
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class with a method.
+    """
+
+    def area(self):
+        """
+        Method that raises an Exception with the message
+        'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
