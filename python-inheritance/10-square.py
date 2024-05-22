e1aa978f3c54b59143cb2fa245883410d8a1d38b
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-22 18:00:50 +0200

    Fix pycodestyle issues

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle that inherits from BaseGeometry.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle shape.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Computes the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            str: A string representation of the rectangle
+            in the format [Rectangle] width/height.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """
+    Represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: A string representation of the square
+            in the format [Square] size/size.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
