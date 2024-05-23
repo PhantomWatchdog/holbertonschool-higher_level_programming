f087085e90d9564e5f88c3b7038c40cd4364b534
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-23 17:04:15 +0200

    Change pop method

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -6,19 +6,19 @@
 class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print("Added {} to the list".format(item))
+        print("Added [{}] to the list".format(item))
 
     def extend(self, item):
         super().extend(item)
-        print("Extended the list with {} items".format(item))
+        print("Extended the list with [{}] items".format(len(item)))
 
     def remove(self, item):
-        print("Removed {} from the list".format(item))
+        print("Removed [{}] from the list".format(item))
         super().remove(item)
 
     def pop(self, index=None):
         if index is None:
-            print("Pop the last item")
-        else:
-            print("Popped {} from the list".format(index))
-        return super().pop(index)
+            index = -1
+        popped = super().pop(index)
+        print("Popped [{}] from the list".format(popped))
+        return popped
