3650a30aec5f43a71f7fdcd67d63e781d10cb359
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-23 16:53:48 +0200

    Deletion of insert method

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
+""" Shapes, Interfaces, and Duck Typing
+"""
 
-import abc from ABC, abstractmethod
 
-
-class Verboselist(list):
+class VerboseList(list):
     def append(self, item):
-        print("Append {}".format(item))
         super().append(item)
+        print("Added {} to the list".format(item))
 
     def extend(self, item):
-        print("Extend {}".format(item))
         super().extend(item)
+        print("Extended the list with {} items".format(item))
 
-    def insert(self, index, item):
-        print("Insert {} at position {}".format(item, index))
-        super().insert(index, item)
+    def remove(self, item):
+        print("Removed {} from the list".format(item))
+        super().remove(item)
+
+    def pop(self, index=None):
+        if index is None:
+            print("Pop the last item")
+        else:
+            print("Popped {} from the list".format(index))
+        return super().pop(index)
