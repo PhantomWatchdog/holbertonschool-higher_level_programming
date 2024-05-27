2af89e1cde102dddb12cb8d2aecd9b2cf232d096
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-27 14:48:50 +0200

    Add chmod

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+save_to_json_file module
+"""
+import json
+
 
 def save_to_json_file(my_obj, filename):
-    import json
+    """
+    Save the given object to a JSON file.
+
+    Args:
+        my_obj: The object to be saved.
+        filename: The name of the file to save the object to.
+
+    Returns:
+        None
+    """
     with open(filename, 'w') as f:
         json.dump(my_obj, f)
