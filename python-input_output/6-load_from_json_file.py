63586a613858258104f715a11da9e7011164e433
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-27 15:27:35 +0200

    Change arch... of code

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""
-Module for load_from_json_file
-"""
-
+"""Module for load_from_json_file"""
+import json
 
 def load_from_json_file(filename):
-    """
-    creates an Object from a “JSON file”
-    """
-    import json
+    """creates an Object from a “JSON file”"""
     with open(filename, 'r') as f:
         return json.load(f)
