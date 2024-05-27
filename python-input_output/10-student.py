0fc8a623cf45b0d95dee6ee006ce31f2166ae130
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-27 16:51:15 +0200

    Add Task 10 files

---
+++ python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """
+    Represents a student with their first name, last name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list): List of attributes to include
+            in the dictionary representation.
+
+            If None, all attributes are included.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
