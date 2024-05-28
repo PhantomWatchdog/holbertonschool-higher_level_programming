37882d4d59ccb1fadac0d854654da836344a542b
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-28 10:39:41 +0200

    Add string format

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,74 +1,25 @@
-#!/usr/bin/python3
-"""
-This module contains a class that demonstrates
-serialization and deserialization using the pickle module.
-"""
 import pickle
 
-
 class CustomObject:
-    """
-    A class representing a custom object.
-
-    Attributes:
-        name (str): The name of the object.
-        age (int): The age of the object.
-        is_student (bool): Indicates whether the object is a student or not.
-    """
     def __init__(self, name, age, is_student):
-        """
-        Initializes a new instance of the MyClass class.
-
-        Args:
-            name (str): The name of the object.
-            age (int): The age of the object.
-            is_student (bool): Indicates whether the object is
-            a student or not.
-        """
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """
-        Display the object's information.
-
-        This method prints the name, age, and student status of the object.
-        """
-        print("Name: {}".format(self.name))
-        print("Age: {}".format(self.age))
-        print("Is Student: {}".format(self.is_student))
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """
-        Serialize the object and save it to a file.
-
-        Args:
-            filename (str): The name of the file to save
-            the serialized object to.
-
-        Returns:
-            None: If the serialization fails.
-        """
         try:
             with open(filename, 'wb') as f:
                 pickle.dump(self, f)
-        except (FileNotFoundError, pickle.PickleError):
+        except Exception:
             return None
 
     @classmethod
     def deserialize(cls, filename):
-        """
-        Deserialize an object from a file.
-
-        Args:
-            filename (str): The name of the file
-            to deserialize the object from.
-
-        Returns:
-            CustomObject: The deserialized object.
-            None: If the deserialization fails.
-        """
         try:
             with open(filename, 'rb') as f:
                 return pickle.load(f)
