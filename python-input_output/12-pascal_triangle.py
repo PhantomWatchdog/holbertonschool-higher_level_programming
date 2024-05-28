1f7a57746b53d838781e8620e8a77437c48d5fd7
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-28 09:15:30 +0200

    Add Task 12 files

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 12-pascal_triangle.py """
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal triangle of n.
+
+    Args:
+        n (int): The number of rows in the Pascal triangle.
+
+    Returns:
+        list: A list of lists representing the Pascal triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
