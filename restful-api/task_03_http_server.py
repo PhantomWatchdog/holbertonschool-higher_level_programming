7f13cec664a0d30ac6ee5a54ca1fd255d17ec4e2
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-30 17:44:56 +0200

    Change /status return

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -28,7 +28,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'{"status": "OK"}')
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
