78890e7fbfee9c3092fe8d0978fc35d3d5edb9e6
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-30 15:01:43 +0200

    Add basic code for Task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello, this is a simple API!")
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
