e01152d612e689547a4fa35a42642bfd804241b2
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-30 17:30:21 +0200

    Change 404 status

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,6 +4,7 @@ import http.server
 
 PORT = 8000
 
+
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -15,23 +16,28 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'{"name": "John", "age": 30, "city": "New York"}')
+            self.wfile.write(b'{"name": "John",\
+                "age": 30, "city": "New York"}')
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
-        else :
+            self.wfile.write(b'{"version": "1.0", \
+                        "description": "A simple API built with http.server"}')
+        else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 Not Found")
 
-def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
+
+def run(server_class=http.server.HTTPServer,
+        handler_class=SimpleHTTPRequestHandler):
     server_address = ('', PORT)
     httpd = server_class(server_address, handler_class)
     print(f"Starting httpd server on port {PORT}")
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
