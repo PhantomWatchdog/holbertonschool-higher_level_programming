23e98614291afb0abf530a5912c849dd67616f94
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-30 17:42:08 +0200

    Change header /data

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,7 +15,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
