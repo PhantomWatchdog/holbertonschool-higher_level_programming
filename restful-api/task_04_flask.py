39788a6f299c3392441fc44ca7f8ff1069361245
PhantomWatchdog <8706@holbertonstudents.com>
2024-05-30 20:34:01 +0200

    Serving JSON data and Expanding API - Successful

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,5 +8,28 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Flask API!"
 
+@app.route('/data')
+def data():
+    users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
+            "john": {"name": "John", "age": 30, "city": "New York"}}
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route('/users/<username>')
+def user(username):
+    users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
+            "john": {"name": "John", "age": 30, "city": "New York"}}
+    user_data = users.get(username)
+    if user_data:
+        user_data["username"] = username
+        return jsonify(user_data)
+    else:
+        return jsonify({"error": "User not found"})
+
+
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=5000)
