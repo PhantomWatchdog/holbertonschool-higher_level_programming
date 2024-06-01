87627b683395660c9b6024eca3462eb884b68970
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-02 01:56:37 +0200

    Test 2

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, \
-    create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 jwt = JWTManager(app)
 auth = HTTPBasicAuth()
-app.config['JWT_SECRET_KEY'] = 'super secret key'
+app.config['JWT_SECRET_KEY'] = 'super_secret_key'
 
 users = {
     "user1": {
@@ -66,27 +65,27 @@ def admin_only():
 
 
 @jwt.unauthorized_loader
-def handle_unauthorized_error(error):
+def handle_unauthorized_error(err):
     return jsonify({"error": "Missing or invalid token"}), 401
 
 
 @jwt.invalid_token_loader
-def handle_invalid_token_error(error):
+def handle_invalid_token_error(err):
     return jsonify({"error": "Invalid token"}), 401
 
 
 @jwt.expired_token_loader
-def handle_expired_token_error(error):
+def handle_expired_token_error(err):
     return jsonify({"error": "Token has expired"}), 401
 
 
 @jwt.revoked_token_loader
-def handle_revoked_token_error(error):
+def handle_revoked_token_error(err):
     return jsonify({"error": "Token has been revoked"}), 401
 
 
 @jwt.needs_fresh_token_loader
-def handle_needs_fresh_token_error(error):
+def handle_needs_fresh_token_error(err):
     return jsonify({"error": "Fresh token required"}), 401
 
 
