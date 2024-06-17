0f6b3dc7e8416dab4b4f127313369bc596c57a6f
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-17 16:00:37 +0200

    Add files for Task 2-5

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Create a table called 'first_table' with the following columns:
+-- 'id' of type integer
+-- 'name' of type text
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
