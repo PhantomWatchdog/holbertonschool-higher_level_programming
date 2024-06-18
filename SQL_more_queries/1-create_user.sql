3acf0b3631cdc971175a26e123fd470ab46beec2
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-18 17:35:24 +0200

    Update Task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create or update the user user_0d_1
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
