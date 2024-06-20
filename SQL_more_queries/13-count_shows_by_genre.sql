1f420d3329f48aa72c60a0c17e072780a2e3ff8a
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-20 11:37:48 +0200

    Completion of Task 12-13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all genres from the database hbtn_0d_tvshows
+-- List all genres from the database hbtn_0d_tvshows and display the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+    FROM tv_genres
+        JOIN tv_show_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
