eeec96952a9e764a2df70b06cfe5632b26b882a6
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-20 15:53:01 +0200

    Remove multiple states in current table

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+def main():
+    """
+    Lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
@@ -16,3 +16,6 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    main()
