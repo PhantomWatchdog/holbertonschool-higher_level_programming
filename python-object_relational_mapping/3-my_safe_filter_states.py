222f13919f6889e16dbeff5ec4f6b9fe43a22eff
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-20 17:22:51 +0200

    Test checker - Good output

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,17 +8,13 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306)
+        db=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name\
-        LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+                   LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
