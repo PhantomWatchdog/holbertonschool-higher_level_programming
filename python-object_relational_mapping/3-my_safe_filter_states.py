689f19899830cd27c67f009609e8b263bb46e213
PhantomWatchdog <8706@holbertonstudents.com>
2024-06-20 17:26:06 +0200

    Test 2

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,15 +6,36 @@ Protect from SQL Injection
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
 
+if __name__ == "__main__":
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name\
-                   LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+
+    # Execute the SQL query
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows
     states = cursor.fetchall()
+
+    # Print the results
     for state in states:
         print(state)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
