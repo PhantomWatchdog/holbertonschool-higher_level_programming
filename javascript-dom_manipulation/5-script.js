a8eb0d4abdce6db456a2fff580893d8e40a0ee86
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-24 09:49:55 +0200

    Task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('header');
+  const updateBtn = document.getElementById('update_header');
+
+  updateBtn.addEventListener('click', function() {
+    header.textContent = 'New Header!!!';
+  });
+});
